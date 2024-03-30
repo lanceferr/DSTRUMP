0b3df520744120d9dafe2057861d7ca3a937f33c
RonnieAJR <ronnieabiogjr@gmail.com>
2024-03-31 01:15:08 +0800

    mp(navigateGrid as function) - working program prior to further changes. dstru_mp - navigate in main, minor changes added

--- DSTRU_MP(navigateGrid as function).c
+++ DSTRU_MP(navigateGrid as function).c
@@ -35,12 +35,12 @@ typedef struct cartesianTag cartesian;
 // insert function prototypes
 
 int getSet_F3(cartesian [][F_COL], cartesian [][F_COL], cartesian [][F_COL]);
-Boolean isOver(cartesian [][C_COL], cartesian [][C_COL], cartesian [][F_COL], int);
+Boolean isOver(cartesian [][C_COL], cartesian [][C_COL], cartesian [][F_COL], cartesian [][P_COL], int);
 void addElement_F(cartesian *, cartesian [][F_COL]);
 void addElement_C(cartesian *, cartesian [][C_COL]);
 int getPatternCompleted(cartesian [][F_COL]);
 int getCardinality_C(cartesian [][C_COL]);
-void displayGrid(cartesian [][F_COL], cartesian [][F_COL], Boolean, cartesian *, int);
+void displayGrid(cartesian [][F_COL], cartesian [][F_COL], Boolean, int, int, int);
 void navigateGrid(cartesian [][F_COL], cartesian [][F_COL], int, int *, int *, cartesian *, Boolean);
 
 
@@ -48,52 +48,33 @@ int
 main()
 {
 	
-	// definition of sets P and S from MP specs for reference
+	// definition of sets P and S from MP specs
+	cartesian set_P[P_ROW][P_COL] = {{{1,1}, {2,2}},  {{1,2}, {2,1}}};
 	
-//	each element corresponds to a quadrant
-//	cartesian set_P[P_ROW][P_COL] = {{{1,1}, {2,2}},  {{1,2}, {2,1}}};
-//	
-
-//	each row corresponds to a pattern in one quadrant
-//	cartesian set_S[S_ROW][S_COL] = {{{1,1}, {1,3}, {2,2}, {3,1}, {3,3}},
-//									 {{4,4}, {4,6}, {5,5}, {6,4}, {6,6}},
-//									 {{1,5}, {2,4}, {2,5}, {2,6}, {3,5}},
-//									 {{4,1}, {4,3}, {5,1}, {5,3}, {6,1}, {6,3}}};
+	cartesian set_S[S_ROW][S_COL] = {{{1,1}, {1,3}, {2,2}, {3,1}, {3,3}},
+									 {{4,4}, {4,6}, {5,5}, {6,4}, {6,6}},
+									 {{1,5}, {2,4}, {2,5}, {2,6}, {3,5}},
+									 {{4,1}, {4,3}, {5,1}, {5,3}, {6,1}, {6,3}}};
 		
-	// variable declarations
-	
-	/*
-		set variables
-		set_C 1-2 contains quadrants in which the pattern is completed by player A and B respectively
-		set_F 1-2 contains the spaces occupied by player A and B respectively
-		set_F3 contains the empty spaces
-	*/
+	// other sets definitions
 	cartesian set_C1[C_ROW][C_COL] = {0}, set_C2[C_ROW][C_COL] = {0};
 	cartesian set_F1[F_ROW][F_COL] = {0}, set_F2[F_ROW][F_COL] = {0}, set_F3[F_ROW][F_COL] = {0};
 
-	/*
-		player position variables
-		pos is the position entered by the player
-		quad_pos is the quadrant where pos is (e.g. top-left <=> (1,1))
-		temp_pos is the indicator where the player is currently in
-	*/
+	// pos definition
+	// (a, b)
 	cartesian pos = {0};
-	cartesian quad_pos = {0};
-	cartesian temp_pos = {0};
 	
-	/*
-		control variables
-		good determines if a move done by the player should be reflected in the grid
-		over determines if the game is over
-		next determines which player is supposed to move
-	*/
+	// (c, d)
+	cartesian quad_pos = {0};
+	
+	int indexRow = 0;
+	int indexCol = 0;
+	
+	// other variable declarations
 	Boolean good = FALSE;
 	Boolean over = FALSE;
 	Boolean next = FALSE;
-	
-	/*
-		
-	*/
+
 	int nF3_Cardinality = getSet_F3(set_F1, set_F2, set_F3);
 	int nF1_PatternCompleted;
 	int nF2_PatternCompleted;
@@ -101,80 +82,76 @@ main()
 	int nC1_Cardinality;
 	int nC2_Cardinality;
 	
-	/*
-		results indicates the winner of the game
-		input contains the character entered by the player while navigating in the grid
-	*/
 	char results[7];
-	char input;
+	
+	// for value checking
+	printf("Set P:\n");
+	for(int i=0; i<P_ROW; i++)
+	{
+		for(int j=0; j<P_COL; j++)
+			printf("(%d, %d)\t", set_P[i][j].x, set_P[i][j].y);
+		printf("\n");
+	}
+	
+	printf("\n\nSet S:\n");
+	for(int i=0; i<S_ROW; i++)
+	{
+		for(int j=0; j<S_COL; j++)
+			printf("(%d, %d)\t", set_S[i][j].x, set_S[i][j].y);
+			
+	printf("\n");
+	}
 	
 	
-	// game start
+//	addElement_F(&set_P[0][0], set_F1);
+//	addElement_F(&set_P[0][1], set_F1);
+//	
+//	addElement_F(&set_P[1][0], set_F2);
+//	addElement_F(&set_P[1][1], set_F2);
+//	
+//	nF3_Cardinality = getSet_F3(set_F1, set_F2, set_F3);
+//	
+//	printf("\n\nSet F3:\n");
+//	for(int i=0; i<F_ROW; i++)
+//	{
+//		for(int j=0; j<F_COL; j++)
+////			printf("(%d, %d)\t", set_F3[i][j].x, set_F3[i][j].y);
+//			if(set_F3[i][j].x == 0)
+//				printf("0\t");
+//			else
+//				printf("1\t");
+//			
+//	printf("\n\n");
+//	}
+	// end of value checking
+	
+	
+
+		
 	do	
 	{
 		
 	//	NextPlayerMove (prototype)
 		
-		// Display the initial gridd
-	    displayGrid(set_F1, set_F2, over, &temp_pos, next);
-	
-	    // loop
-	    do { 
-	        // Check if a key has been pressed
-//	        if (kbhit()) {
-	            input = getch(); // Get the pressed key
-	            switch (input) 
-	            {
-	                case 'W':
-	                case 'w':
-	                    if (temp_pos.x > 0) 
-	                        temp_pos.x--;
-	                    break;
-	                case 'A':
-	                case 'a':
-	                    if (temp_pos.y > 0) 
-	                	    temp_pos.y--;
-	                    break;
-	                case 'S':
-	                case 's':
-	                    if (temp_pos.x < F_ROW - 1) 
-	                        temp_pos.x++;
-	                    break;
-	                case 'D':
-	                case 'd':
-	                    if (temp_pos.y < F_COL - 1) 
-	                    	temp_pos.y++;
-	                    break;
-	
-	                case '\r': // Enter key
-	               		pos = temp_pos;
-	                    
-	                    if (set_F3[pos.x][pos.y].x != 0) 
-	                    {                   
-							good = !good;
-							
-							if(next)
-								addElement_F(&pos, set_F1);	
-								
-							else if(!next)
-								addElement_F(&pos, set_F2);		
-	                    }
-	                    	
-	                
-//	                default:
-//	                    break;
-	            }
-	
-	            // Display the updated grid
-	            displayGrid(set_F1, set_F2, over, &temp_pos, next);
-	            
-//	        }
-	    }while (input != '\r');
-		input = '\0';
+		navigateGrid(set_F1, set_F2, over, &indexRow, &indexCol, &pos, next);
 		
-		// get (c,d)
-		quad_pos.x = (pos.x)/3 ;
-		quad_pos.y = (pos.y)/3 ;	
+		// get (c,d)		
+		quad_pos.x = (pos.x-1)/3 +1;
+		quad_pos.y = (pos.y-1)/3 +1;
+
+		// then...
+		
+//		if(!over && next && (set_F3[pos.x+1][pos.y+1].x != 0 ) )
+//		{
+//			good = !good;
+//			addElement_F(&pos, set_F1);
+//		}
+//		
+//		else if(!over && !next && (set_F3[pos.x+1][pos.y+1].x != 0 ) )
+//		{
+//			good = !good;
+//			addElement_F(&pos, set_F2);
+//		}		
 		
 		nF3_Cardinality = getSet_F3(set_F1, set_F2, set_F3);
 		
@@ -188,10 +165,10 @@ main()
 		nC1_Cardinality = getCardinality_C(set_C1);
 		nC2_Cardinality = getCardinality_C(set_C2);
 		
-		if(good && next && (nF1_PatternCompleted > nC1_Cardinality))
+		if(!over /*&& good*/ && next && (nF1_PatternCompleted > nC1_Cardinality))
 			addElement_C(&quad_pos, set_C1);
 			
-		else if(good && !next && (nF2_PatternCompleted > nC2_Cardinality))
+		else if(!over /*&& good*/ && !next && (nF2_PatternCompleted > nC2_Cardinality))
 			addElement_C(&quad_pos, set_C2);
 			
 		
@@ -199,7 +176,7 @@ main()
 			good = !good;
 		
 		
-		over = isOver(set_C1, set_C2, set_F3, nF3_Cardinality);
+		over = isOver(set_C1, set_C2, set_F3, set_P, nF3_Cardinality);
 		
 	// GameOver (prototype)	
 		
@@ -214,7 +191,7 @@ main()
 			strcpy(results, "B wins");
 		
 		else if(over)
-			strcpy(results, "Draw");
+			strcpy(results, "No win");
 		
 		if(!over)
 			next = !next;
@@ -222,7 +199,7 @@ main()
 	}while(!over);
 	
 	
-	displayGrid(set_F1, set_F2, over, &temp_pos, next);
+	displayGrid(set_F1, set_F2, over, indexRow, indexCol, next);
 	printf("\n\n%s\n", results);
 	
 	for(int i=0; i<2; i++)
@@ -274,7 +251,7 @@ getSet_F3(cartesian set_F1[][F_COL], cartesian set_F2[][F_COL], cartesian set_F3
 // determines if over
 Boolean
 isOver(cartesian set_C1[][C_COL], cartesian set_C2[][C_COL],
-	   cartesian set_F3[][F_COL], int nF3_Cardinality)
+	   cartesian set_F3[][F_COL], cartesian set_P[][P_COL], int nF3_Cardinality)
 {
 	Boolean bCon1 = FALSE,
 			bCon2 = FALSE,
@@ -309,29 +286,27 @@ isOver(cartesian set_C1[][C_COL], cartesian set_C2[][C_COL],
 // adds an element to a set that is a subset of set F
 void
 addElement_F(cartesian * sElement, cartesian sSet[][F_COL])
-{	
+{
+	int xi = sElement->x -1;
+	int yi = sElement->y -1;
+	
 	// elements are stored according to their values:
 	// x = row		y = col	
-	if(sSet[sElement->x][sElement->y].x == 0)
-	{
-		sSet[sElement->x][sElement->y].x = sElement->x + 1;
-		sSet[sElement->x][sElement->y].y = sElement->y + 1;
-		
-	}
+	if(sSet[xi][yi].x == 0)
+		sSet[xi][yi] = *sElement;
 }
 
 // adds an element to a set that is a subset of set C
 void
 addElement_C(cartesian * sElement, cartesian sSet[][C_COL])
 {
+	int xi = sElement->x -1;
+	int yi = sElement->y -1;
+	
 	// elements are stored according to their values:
-	// x = row		y = col	
-	if(sSet[sElement->x][sElement->y].x == 0)
-	{
-		sSet[sElement->x][sElement->y].x = sElement->x + 1;
-		sSet[sElement->x][sElement->y].y = sElement->y + 1;
-		
-	}
+	// x = row		y = col
+	if(sSet[xi][yi].x == 0)
+		sSet[xi][yi] = *sElement;
 }
 
 // gets the cardinality of the (power set of F) intersection (set S)
@@ -381,7 +356,7 @@ getCardinality_C(cartesian sSet[][C_COL])
 {
 	int i, k, nCardinality=0;
 	
-	// checks if the space is not empty
+	// checks if the coordinate is not empty
 	for(i=0; i<C_ROW; i++)
 		for(k=0; k<C_COL; k++)
 			if(sSet[i][k].x != 0)
@@ -391,12 +366,10 @@ getCardinality_C(cartesian sSet[][C_COL])
 }
 
 // Function to display the grid
-void displayGrid(cartesian set_F1[][F_COL], cartesian set_F2[][F_COL], Boolean over, cartesian *temp_pos, int next) 
+void displayGrid(cartesian set_F1[][F_COL], cartesian set_F2[][F_COL], Boolean over, int indexRow, int indexCol, int next) 
 {
+    system("cls"); // Clear the screen (Windows specific)
     int i, j;
-    
-    // clear the screen
-    system("cls");
     
     if(over)
     {
@@ -404,38 +377,30 @@ void displayGrid(cartesian set_F1[][F_COL], cartesian set_F2[][F_COL], Boolean o
 	}
 	else
 	{
-		// 85
-		printf("\n\n\t\t\\vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv/\n");
-		printf("\t\t>                                                                                   <\n");
-	    printf("\t\t>                       WELCOME TO THE DSTRU OF DEATH GAME >:)                      <\n");
-	    printf("\t\t>                                                                                   <\n");
-	    printf("\t\t/^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^\\\n\n");
-	    
-	    printf("\t\tpress WASD to navigate and press ENTER to place your position\n");
-	    printf("\t\t| * | current position\n\n");
+	    printf("WELCOME TO THE DSTRU OF DEATH GAME >:)\n");
+	    printf("press WASD to navigate and press ENTER place your position\n");
 	    if(next == 0)
-	    printf("\t\tPlayer B turn\n\n\n");
+	    printf("\nplayer 2 turn\n\n");
 	    else if(next == 1)
-	    printf("\t\tPlayer A turn\n\n\n");
+	    printf("\nplayer 1 turn\n\n");
 	}
 
     for (i = 0; i < F_ROW; i++) {
         for (j = 0; j < F_COL; j++) {
-            if (i == temp_pos->x && j == temp_pos->y && !over)
-                printf("\t\t| * |"); // Player position
+            if (i == indexRow && j == indexCol && !over)
+                printf("\t|*|"); // Player position
             else if(set_F1[i][j].x != 0)
-                printf("\t\t| A |"); // Box status
+                printf("\t|A|"); // Box status
             else if(set_F2[i][j].x != 0)
-                printf("\t\t| B |"); // Box status
+                printf("\t|B|"); // Box status
             else
-            	printf("\t\t| . |");
+            	printf("\t|.|");
             
         }
         printf("\n\n");
     }
 }
 
-/*
 void
 navigateGrid(cartesian set_F1[][F_COL], cartesian set_F2[][F_COL], Boolean over,
 		     int *indexRow, int *indexCol, cartesian *pos, Boolean next)
@@ -501,4 +466,3 @@ navigateGrid(cartesian set_F1[][F_COL], cartesian set_F2[][F_COL], Boolean over,
         }
     }while (input != '\r');
 }
-*/
